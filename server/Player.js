bda523b03651740eb3a9f32818a71c3dfc65faa3
Oriol Ylla <o.yllacatala@ocado.com>
2019-12-25 23:23:11 +0100

    roll functionality

--- server/Player.js
+++ server/Player.js
@@ -1,17 +1,17 @@
 const uuidv4 = require("uuid/v4")
 
 function Player({
-    name = 'Guest' + rand(9999),
-    id = uuidv4(),
-    score = 0,
+  name = 'Guest' + rand(9999),
+  id = uuidv4(),
+  score = 0,
 }) {
-    this.name = name;
-    this.id = id;
-    this.score = score;
+  this.name = name;
+  this.id = id;
+  this.score = score;
 };
 
-function rand (n) {
-    return Math.floor(Math.random() * n+1)
+function rand(n) {
+  return Math.floor(Math.random() * n + 1)
 }
 
 module.exports = Player;
